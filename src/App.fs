e18af0c120b7476f4ffda4b0da215e109cfa9677
Pavan Narendra <pavan.narendra@jet.com>
2019-08-02 14:55:35 -0400

    Using Elmish

--- src/App.fs
+++ src/App.fs
@@ -1,3 +1,43 @@
 module App
 
-printfn "Hello from Fable"
+open Fable.React
+open Fable.React.Props
+
+type Model =
+    {
+        Count: int
+    }
+
+ type Msg=
+    | Increment
+    | Decrement
+    | IncrementBy of int
+    | Reset
+
+let init () = 
+    {Count = 42}
+
+let update msg model : Model =
+    match msg with
+    | Increment -> {model with Count = model.Count + 1}
+    | Decrement -> {model with Count = model.Count - 1}
+    | IncrementBy step -> {model with Count = model.Count + step}
+    | Reset -> init()
+
+let view model dispatch = 
+    div [] 
+        [ 
+            button [ OnClick ( fun _ -> dispatch Decrement ) ] [ str "-" ]
+            h1 [] [ ofInt model.Count ]
+            button [ OnClick ( fun _ -> dispatch Increment ) ] [ str "+" ]
+            button [ OnClick ( fun _ -> dispatch (IncrementBy 5) ) ] [ str "+5" ]
+            button [ OnClick ( fun _ -> dispatch (Reset) ) ] [ str "Reset" ]
+            ul [] [ for i in 1..5 -> li [] [ofInt i]]
+        ]
+
+open Elmish
+open Elmish.React
+
+Program.mkSimple init update view 
+|> Program.withReactSynchronous "app"
+|> Program.run
